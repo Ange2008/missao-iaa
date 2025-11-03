5fc85ec3fb1838dc42506a789e1afd63ea071c2e
mariasemangela <140639757+Ange2008@users.noreply.github.com>
2025-11-03 09:47:16 -0300

    03112025

---
+++ java/perguntas.js
@@ -0,0 +1,12 @@
+export const perguntas = [
+    {
+    enunciado: "O crescente poder das redes sociais e dos influenciadores digitais na vida cotidiana levanta um dilema significativo sobre o impacto dessa dinâmica na sociedade e nos indivíduos.";
+    alternativas:[
+        {
+            texto: "As redes sociais e os influenciadores democratizam o acesso à informação e à voz, permitindo que minorias e causas sociais ganhem visibilidade global. Além disso, criam novas oportunidades econômicas para empreendedores e facilitam a conexão autêntica entre comunidades com interesses em comum, promovendo engajamento cívico e social."
+            afirmacao:[
+                "A influência digital é essencialmente benéfica, pois atua como um poderoso catalisador para o empoderamento social e econômico, oferecendo uma plataforma global para vozes marginalizadas e fomentando a criação de uma "creator economy" inovadora."
+            ],
+        }
+    ]
+]
