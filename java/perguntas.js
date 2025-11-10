248ad23b85b64f0ce35873ca5d7ca66411a6d16b
mariasemangela <140639757+Ange2008@users.noreply.github.com>
2025-11-10 09:50:45 -0300

    098765t43

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,6 +1,6 @@
 export const perguntas = [
     {
-      enunciado: "O crescente poder das redes sociais e dos influenciadores digitais na vida cotidiana levanta um dilema significativo sobre o impacto dessa dinâmica na sociedade e nos indivíduos.";
+      enunciado: "O crescente poder das redes sociais e dos influenciadores digitais na vida cotidiana levanta um dilema significativo sobre o impacto dessa dinâmica na sociedade e nos indivíduos."
       alternativas:[
         {
             texto: "As redes sociais e os influenciadores democratizam o acesso à informação e à voz, permitindo que minorias e causas sociais ganhem visibilidade global. Além disso, criam novas oportunidades econômicas para empreendedores e facilitam a conexão autêntica entre comunidades com interesses em comum, promovendo engajamento cívico e social."
